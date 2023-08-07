2961a89d2177a7c26fe28fc23bd06a646aae36b3
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 19:03:06 +0700

    Add Nowrap Text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,9 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title fw-bold text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; text-shadow: 2px 2px #000000">SIPERA</h1>
-                    <p class="page-header-ui-text fw-bold mb-5 text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; text-shadow: 2px 2px #000000">Sistem Informasi Pelaporan Capaian Kinerja</p>
-                    <p class="page-header-ui-text fw-bold mb-5 text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; text-shadow: 2px 2px #000000">Kecamatan Pancoran Mas</p>
+                    <h1 class="page-header-ui-title fw-bold text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; white-space: nowrap; text-shadow: 2px 2px #000000">SIPERA</h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; white-space: nowrap; text-shadow: 2px 2px #000000">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white" style="background-color: rgba(128, 128, 128, 0.5); display: inline-block; white-space: nowrap; text-shadow: 2px 2px #000000">Kecamatan Pancoran Mas</p>
                     @auth
                     <a class="btn btn-teal fw-bold me-2" href="">Masuk Sistem</a>
                     @endauth
