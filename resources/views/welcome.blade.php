d16db56d261197815ce12461f651ccefd8c44ff3
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:45:41 +0700

    Text Background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title fw-bold text-white">SIPERA</h1>
-                    <p class="page-header-ui-text fw-bold mb-5 text-white">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    <h1 class="page-header-ui-title fw-bold text-white bg-primary">SIPERA</h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white bg-primary">Sistem Informasi Pelaporan Capaian Kinerja</p>
                     @auth
                     <a class="btn btn-teal fw-bold me-2" href="">Masuk Sistem</a>
                     @endauth
-                    <a class="btn btn-link fw-bold text-white" href="#commodity-price">Jelajahi</a>
+                    <a class="btn btn-link fw-bold text-white" href="">Jelajahi</a>
                 </div>
             </div>
         </div>
