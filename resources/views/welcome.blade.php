a00dc9fc7ac330730dff02bfb873b8b55e49fecd
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 19:18:41 +0700

    Add Rounded Bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,17 @@
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
                     <h1 class="page-header-ui-title fw-bold text-white">
-                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; text-shadow: 2px 2px #000000">
+                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; border-radius: 10px; text-shadow: 2px 2px #000000">
                             SIPERA
                         </span>
                     </h1>
                     <p class="page-header-ui-text fw-bold mb-5 text-white">
-                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; text-shadow: 2px 2px #000000">
+                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; border-radius: 10px; text-shadow: 2px 2px #000000">
                             Sistem Informasi Pelaporan Capaian Kinerja
                         </span>
                     </p>
                     <p class="page-header-ui-text fw-bold mb-5 text-white">
-                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; text-shadow: 2px 2px #000000">
+                        <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; border-radius: 10px; text-shadow: 2px 2px #000000">
                             Kecamatan Pancoran Mas
                         </span>
                     </p>
