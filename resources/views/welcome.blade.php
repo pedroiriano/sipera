a8c0e1d5cdf13bc641e18a32452d35dfed2ecf34
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:42:04 +0700

    Change Text Color and Weight

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title text-black">SIPERA</h1>
-                    <p class="page-header-ui-text mb-5 text-black">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    <h1 class="page-header-ui-title fw-bold text-white">SIPERA</h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white">Sistem Informasi Pelaporan Capaian Kinerja</p>
                     @auth
-                    <a class="btn btn-teal fw-500 me-2" href="">Masuk Sistem</a>
+                    <a class="btn btn-teal fw-bold me-2" href="">Masuk Sistem</a>
                     @endauth
-                    <a class="btn btn-link fw-500 text-black" href="#commodity-price">Jelajahi</a>
+                    <a class="btn btn-link fw-bold text-white" href="#commodity-price">Jelajahi</a>
                 </div>
             </div>
         </div>
