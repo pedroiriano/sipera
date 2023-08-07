aab893b664bf7a91177fd042b12c81d1f1d729a9
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:39:45 +0700

    Rollback

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,16 @@
 <!-- Page Header-->
 <header class="page-header-ui page-header-ui-light" style="background-image: url({{ asset('images/bg/monitoring.png') }}); background-size: auto 100%">
     <div class="page-header-ui-content">
-        <div class="container">
-            <div class="row justify-content-end">
-                <div class="col-xl-8 col-lg-10 text-end">
-                    <h1 class="page-header-ui-title text-black"></h1>
-                    <p class="page-header-ui-text mb-5 text-black"></p>
-                    <a class="btn btn-link fw-500 text-black"></a>
+        <div class="container px-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-xl-8 col-lg-10 text-center">
+                    @include('inc.alert-message')
+                    <h1 class="page-header-ui-title text-black">SIPERA</h1>
+                    <p class="page-header-ui-text mb-5 text-black">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    @auth
+                    <a class="btn btn-teal fw-500 me-2" href="">Masuk Sistem</a>
+                    @endauth
+                    <a class="btn btn-link fw-500 text-black" href="#commodity-price">Jelajahi</a>
                 </div>
             </div>
         </div>
