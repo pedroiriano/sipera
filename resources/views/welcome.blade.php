17d0a3c1a0b04e6422352d2f8676a8877fe2d569
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:37:09 +0700

    Delete Content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,6 @@
 @section('content')
 <!-- Page Header-->
 <header class="page-header-ui page-header-ui-light" style="background-image: url({{ asset('images/bg/monitoring.png') }}); background-size: auto 100%">
-    <div class="page-header-ui-content">
-        <div class="container">
-            <div class="row justify-content-end">
-                <div class="col-xl-8 col-lg-10 text-end">
-                    @include('inc.alert-message')
-                    <h1 class="page-header-ui-title text-black">SIPERA</h1>
-                    <p class="page-header-ui-text mb-5 text-black">Sistem Informasi Pelaporan Capaian Kinerja</p>
-                    @auth
-                    <a class="btn btn-teal fw-500 me-2" href="">Masuk Sistem</a>
-                    @endauth
-                    <a class="btn btn-link fw-500 text-black" href="#commodity-price">Jelajahi</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{-- Hero Image --}}
 </header>
 @endsection
