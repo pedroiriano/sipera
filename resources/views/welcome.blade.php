536a014b2c003be3db3949a48fb518eff66426ff
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:38:54 +0700

    Delete Text in Header Content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,16 @@
 @section('content')
 <!-- Page Header-->
 <header class="page-header-ui page-header-ui-light" style="background-image: url({{ asset('images/bg/monitoring.png') }}); background-size: auto 100%">
-    {{-- Hero Image --}}
+    <div class="page-header-ui-content">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-xl-8 col-lg-10 text-end">
+                    <h1 class="page-header-ui-title text-black"></h1>
+                    <p class="page-header-ui-text mb-5 text-black"></p>
+                    <a class="btn btn-link fw-500 text-black"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
 @endsection
