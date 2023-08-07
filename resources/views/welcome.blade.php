ca75a5e326b71836ca79ce5ce9b4c8231eabea1e
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:51:45 +0700

    Text Shadow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title fw-bold text-white text-shadow">SIPERA</h1>
-                    <p class="page-header-ui-text fw-bold mb-5 text-white text-shadow">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    <h1 class="page-header-ui-title fw-bold text-white" style="text-shadow: 2px 2px #000000">SIPERA</h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white">Sistem Informasi Pelaporan Capaian Kinerja</p>
                     @auth
                     <a class="btn btn-teal fw-bold me-2" href="">Masuk Sistem</a>
                     @endauth
