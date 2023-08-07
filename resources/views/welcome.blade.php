8a2de0bdf9f9eee81d6a343f6304d9cf75a148e2
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-07 18:46:35 +0700

    Add Badge Class to Text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title fw-bold text-white bg-primary">SIPERA</h1>
-                    <p class="page-header-ui-text fw-bold mb-5 text-white bg-primary">Sistem Informasi Pelaporan Capaian Kinerja</p>
+                    <h1 class="page-header-ui-title fw-bold text-white bg-primary badge">SIPERA</h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-white bg-primary badge">Sistem Informasi Pelaporan Capaian Kinerja</p>
                     @auth
                     <a class="btn btn-teal fw-bold me-2" href="">Masuk Sistem</a>
                     @endauth
