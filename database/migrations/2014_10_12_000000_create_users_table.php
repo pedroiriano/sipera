c2dc0964a8668ab992a58438dd078c4f8803a662
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-11 22:20:20 +0700

    Fix User Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('role_id');
+            $table->bigInteger('district_id')->nullable();
+            $table->string('status')->default('Tidak Aktif');
             $table->rememberToken();
             $table->timestamps();
         });
