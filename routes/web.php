9a8c627942d4fca30f8613aa0f00cf1dc82ed4ec
Pedro Iriano <pdzeus83aw@gmail.com>
2023-08-11 22:26:57 +0700

    Make Auth

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
 Route::get('/', [PagesController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
