744cd0fa27ab52fbdb23b0d18ab4057adfa04608
Pedro Iriano <pdzeus83aw@gmail.com>
2023-09-03 12:36:32 +0700

    Makes Responsive Table

--- resources/views/backend/monitoring/performance-result.blade.php
+++ resources/views/backend/monitoring/performance-result.blade.php
@@ -41,102 +41,110 @@
             </div>
         </div>
         <div class="card-body">
-            <table id="datatablesPrint">
-                <thead>
-                    <tr>
-                        <th class="text-center">Nama Kegiatan</th>
-                        <th class="text-center">Target Kinerja Kegiatan</th>
-                        <th class="text-center">Realisasi</th>
-                        <th class="text-center">Fisik (%)</th>
-                        <th class="text-center">Kategori Masalah</th>
-                        <th class="text-center">Rincian Masalah</th>
-                        <th class="text-center">Bulan</th>
-                        <th class="text-center">Tahun</th>
-                        <th class="text-center">Perangkat Daerah</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                    <tr>
-                        <th class="text-center">Nama Kegiatan</th>
-                        <th class="text-center">Target Kinerja Kegiatan</th>
-                        <th class="text-center">Realisasi</th>
-                        <th class="text-center">Fisik (%)</th>
-                        <th class="text-center">Kategori Masalah</th>
-                        <th class="text-center">Rincian Masalah</th>
-                        <th class="text-center">Bulan</th>
-                        <th class="text-center">Tahun</th>
-                        <th class="text-center">Perangkat Daerah</th>
-                    </tr>
-                </tfoot>
-                <tbody>
-                    @if (((auth()->user()->role_id) == 1) || ((auth()->user()->role_id) == 2))
-                        @if (count($reas) > 0)
-                            @foreach ($reas as $rea)
-                                @php
-                                    switch ($rea->month) {
-                                        case 1:
-                                            $month_string = 'Januari';
-                                            break;
-                                        case 2:
-                                            $month_string = 'Februari';
-                                            break;
-                                        case 3:
-                                            $month_string = 'Maret';
-                                            break;
-                                        case 4:
-                                            $month_string = 'April';
-                                            break;
-                                        case 5:
-                                            $month_string = 'Mei';
-                                            break;
-                                        case 6:
-                                            $month_string = 'Juni';
-                                            break;
-                                        case 7:
-                                            $month_string = 'Juli';
-                                            break;
-                                        case 8:
-                                            $month_string = 'Agustus';
-                                            break;
-                                        case 9:
-                                            $month_string = 'September';
-                                            break;
-                                        case 10:
-                                            $month_string = 'Oktober';
-                                            break;
-                                        case 11:
-                                            $month_string = 'November';
-                                            break;
-                                        case 12:
-                                            $month_string = 'Desember';
-                                            break;
-                                        default:
-                                            $month_string = 'Terjadi Kesalahan Data';
-                                            break;
-                                    }
-                                @endphp
-                                <tr>
-                                    <td>{{ $rea->sub_activity }}</td>
-                                    <td>{{ $rea->physic }}</td>
-                                    <td>{{ $rea->physic_use }}</td>
-                                    <td>{{ $rea->performance }}</td>
-                                    <td>{{ $rea->problem_category }}</td>
-                                    <td>{{ $rea->problem_description }}</td>
-                                    <td>{{ $month_string }}</td>
-                                    <td>{{ $rea->year }}</td>
-                                    <td>{{ $rea->name }}</td>
-                                </tr>
-                            @endforeach
-                        @else
+            <div class="table-responsive">
+                <table id="datatablesPrint">
+                    <thead>
                         <tr>
-                            <td colspan="9" class="text-center">Data Masih Kosong</td>
+                            <th class="text-center">Nama Kegiatan</th>
+                            <th class="text-center">Anggaran</th>
+                            <th class="text-center">Rencana Anggaran</th>
+                            <th class="text-center">Realisasi</th>
+                            <th class="text-center">Kesesuaian Pelaksanaan (%)</th>
+                            <th class="text-center">Kategori Masalah</th>
+                            <th class="text-center">Rincian Masalah</th>
+                            <th class="text-center">Solusi</th>
+                            <th class="text-center">Bulan</th>
+                            <th class="text-center">Tahun</th>
+                            <th class="text-center">Perangkat Daerah</th>
                         </tr>
-                        @endif
-                    @else
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th class="text-center">Nama Kegiatan</th>
+                            <th class="text-center">Anggaran</th>
+                            <th class="text-center">Rencana Anggaran</th>
+                            <th class="text-center">Realisasi</th>
+                            <th class="text-center">Kesesuaian Pelaksanaan (%)</th>
+                            <th class="text-center">Kategori Masalah</th>
+                            <th class="text-center">Rincian Masalah</th>
+                            <th class="text-center">Solusi</th>
+                            <th class="text-center">Bulan</th>
+                            <th class="text-center">Tahun</th>
+                            <th class="text-center">Perangkat Daerah</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @if (((auth()->user()->role_id) == 1) || ((auth()->user()->role_id) == 2))
+                            @if (count($reas) > 0)
+                                @foreach ($reas as $rea)
+                                    @php
+                                        switch ($rea->month) {
+                                            case 1:
+                                                $month_string = 'Januari';
+                                                break;
+                                            case 2:
+                                                $month_string = 'Februari';
+                                                break;
+                                            case 3:
+                                                $month_string = 'Maret';
+                                                break;
+                                            case 4:
+                                                $month_string = 'April';
+                                                break;
+                                            case 5:
+                                                $month_string = 'Mei';
+                                                break;
+                                            case 6:
+                                                $month_string = 'Juni';
+                                                break;
+                                            case 7:
+                                                $month_string = 'Juli';
+                                                break;
+                                            case 8:
+                                                $month_string = 'Agustus';
+                                                break;
+                                            case 9:
+                                                $month_string = 'September';
+                                                break;
+                                            case 10:
+                                                $month_string = 'Oktober';
+                                                break;
+                                            case 11:
+                                                $month_string = 'November';
+                                                break;
+                                            case 12:
+                                                $month_string = 'Desember';
+                                                break;
+                                            default:
+                                                $month_string = 'Terjadi Kesalahan Data';
+                                                break;
+                                        }
+                                    @endphp
+                                    <tr>
+                                        <td>{{ $rea->sub_activity }}</td>
+                                        <td>{{ $rea->budget }}</td>
+                                        <td>{{ $rea->physic }}</td>
+                                        <td>{{ $rea->physic_use }}</td>
+                                        <td>{{ $rea->performance }}</td>
+                                        <td>{{ $rea->problem_category }}</td>
+                                        <td>{{ $rea->problem_description }}</td>
+                                        <td>{{ $rea->problem_solution }}</td>
+                                        <td>{{ $month_string }}</td>
+                                        <td>{{ $rea->year }}</td>
+                                        <td>{{ $rea->name }}</td>
+                                    </tr>
+                                @endforeach
+                            @else
+                            <tr>
+                                <td colspan="11" class="text-center">Data Masih Kosong</td>
+                            </tr>
+                            @endif
+                        @else
 
-                    @endif
-                </tbody>
-            </table>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
