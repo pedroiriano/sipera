acfe40adc6991436b3a57f2e1ca8e8592b8e8deb
Pedro Iriano <pdzeus83aw@gmail.com>
2023-09-08 06:03:33 +0700

    Manual Book

--- resources/views/inc/frontend-nav.blade.php
+++ resources/views/inc/frontend-nav.blade.php
@@ -14,47 +14,19 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto me-lg-5">
                 <li class="nav-item"><a class="nav-link" href="/">Beranda</a></li>
-                <li class="nav-item dropdown dropdown-xl no-caret">
+                <li class="nav-item dropdown dropdown-md no-caret">
                     <a class="nav-link dropdown-toggle" id="navbarDropdownDemos" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Kinerja
+                        Panduan
                         <i class="fas fa-chevron-right dropdown-arrow"></i>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end animated--fade-in-up me-lg-n25 me-xl-n15" aria-labelledby="navbarDropdownDemos">
                         <div class="row g-0">
-                            <div class="col-lg-5 p-lg-3 bg-img-cover overlay overlay-primary overlay-70 d-none d-lg-block" style="background-image: url('assets/img/backgrounds/bg-dropdown-xl.jpg')">
-                                <div class="d-flex h-100 w-100 align-items-center justify-content-center">
-                                    <div class="text-white text-center z-1">
-                                        <div class="mb-3">Semua Monitoring Kegiatan Dapat Dilihat Di Sini:</div>
-                                        <a class="btn btn-white btn-sm text-primary fw-500" href="/">Lihat Semua</a>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="col-lg-7 p-lg-5">
                                 <div class="row">
                                     <div class="col-lg-12">
-                                        <h6 class="dropdown-header text-primary">Realisasi Fisik dan Keuangan</h6>
-                                        {{-- @if (count($frontNavCats) > 0)
-                                            @foreach ($frontNavCats as $frontNavCat)
-                                                <a class="dropdown-item" href="/">{{ $frontNavCat->name }}</a>
-                                            @endforeach
-                                        @endif --}}
+                                        <h6 class="dropdown-header text-primary">Buku Penggunaan</h6>
+                                            <a class="dropdown-item" href="{{ asset('download/book.pdf') }}" target="_blank">Unduh</a>
                                         <div class="dropdown-divider border-0 d-lg-none"></div>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <h6 class="dropdown-header text-primary">Permasalahan Penyerapan Anggaran</h6>
-                                        {{-- @if (count($frontNavMars) > 0)
-                                            @foreach ($frontNavMars as $frontNavMar)
-                                                <a class="dropdown-item" href="/">{{ $frontNavMar->name }}</a>
-                                            @endforeach
-                                        @endif --}}
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <h6 class="dropdown-header text-primary">Capaian Kinerja</h6>
-                                        {{-- @if (count($frontNavMars) > 0)
-                                            @foreach ($frontNavMars as $frontNavMar)
-                                                <a class="dropdown-item" href="/">{{ $frontNavMar->name }}</a>
-                                            @endforeach
-                                        @endif --}}
                                     </div>
                                 </div>
                             </div>
