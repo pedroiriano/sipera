848ec5912fbb3a9e7453fbd0ef9b912769d79bfc
Pedro Iriano <pdzeus83aw@gmail.com>
2023-09-08 06:10:27 +0700

    Change Text Color

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,7 +12,7 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-xl-8 col-lg-10 text-center">
                     @include('inc.alert-message')
-                    <h1 class="page-header-ui-title fw-bold text-white">
+                    {{-- <h1 class="page-header-ui-title fw-bold text-white">
                         <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; border-radius: 10px; text-shadow: 2px 2px #000000">
                             SIPERA
                         </span>
@@ -24,6 +24,21 @@
                     </p>
                     <p class="page-header-ui-text fw-bold mb-5 text-white">
                         <span style="background-color: rgba(128, 128, 128, 0.75); display: inline; border-radius: 10px; text-shadow: 2px 2px #000000">
+                            Kecamatan Pancoran Mas
+                        </span>
+                    </p> --}}
+                    <h1 class="page-header-ui-title fw-bold text-black">
+                        <span style="background-color: rgba(128, 128, 128, 0.5); display: inline; border-radius: 10px">
+                            SIPERA
+                        </span>
+                    </h1>
+                    <p class="page-header-ui-text fw-bold mb-5 text-black">
+                        <span style="background-color: rgba(128, 128, 128, 0.5); display: inline; border-radius: 10px">
+                            Sistem Informasi Pelaporan Capaian Kinerja
+                        </span>
+                    </p>
+                    <p class="page-header-ui-text fw-bold mb-5 text-black">
+                        <span style="background-color: rgba(128, 128, 128, 0.5); display: inline; border-radius: 10px">
                             Kecamatan Pancoran Mas
                         </span>
                     </p>
